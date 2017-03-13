77650bbceab1b3fa5ec4d2cce414ede37d79c834
lxm065 <151488669@qq.com>
2017-03-14 00:39:07 +0800

    wtf

---
+++ Account/156917776#3/SavedVariables/Accountant_Classic.lua
@@ -0,0 +1,720 @@
+
+Accountant_SaveData = nil
+Accountant_ClassicSaveData = {
+	["火羽山"] = {
+		["悠悠乳"] = {
+			["options"] = {
+				["dateformat"] = 1,
+				["showbutton"] = true,
+				["date"] = "05/03/17",
+				["showmoneyinfo"] = false,
+				["showmoneyonbutton"] = true,
+				["LDBDisplaySessionInfo"] = false,
+				["tracksubzone"] = true,
+				["lastsessiondate"] = "05/03/17",
+				["showintrotip"] = true,
+				["buttonpos"] = 150,
+				["weekstart"] = 1,
+				["version"] = "2.08.07",
+				["weekdate"] = "",
+				["cross_server"] = true,
+				["month"] = "03",
+				["curryear"] = "2017",
+				["moneyinfoframe_y"] = -80,
+				["moneyinfoframe_x"] = 10,
+				["prvday"] = "02/03/17",
+				["trackzone"] = true,
+				["showsessiononbutton"] = true,
+				["faction"] = "Horde",
+				["totalcash"] = 3760381,
+				["class"] = "MAGE",
+				["prvdateweek"] = "02/26/17",
+				["dateweek"] = "03/05/17",
+			},
+			["data"] = {
+				["TRAIN"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["BARBER"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["TAXI"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["GUILD"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["OTHER"] = {
+					["PrvWeek"] = {
+						["In"] = 3760381,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 3760381,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 3760381,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 3760381,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 3760381,
+						["Out"] = 0,
+					},
+				},
+				["LFG"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["TRADE"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["AH"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["VOID"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["MAIL"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["GARRISON"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["LOOT"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["MERCH"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["TRANSMO"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["REPAIRS"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+				["QUEST"] = {
+					["PrvWeek"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvDay"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Year"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Week"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Day"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Month"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvYear"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["PrvMonth"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Session"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+					["Total"] = {
+						["In"] = 0,
+						["Out"] = 0,
+					},
+				},
+			},
+		},
+	},
+}
+Accountant_ClassicDB = {
+	["profileKeys"] = {
+		["悠悠乳 - 火羽山"] = "Default",
+	},
+	["profiles"] = {
+		["Default"] = {
+		},
+	},
+}
+Accountant_Classic_NewDB = nil
